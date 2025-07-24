77440b75522b36ce16e92fffe4763de191d8ccb8
Asadbek Shariyorov <asadbekshaxriyorov2004@gmail.com>
2025-07-24 10:45:29 +0500

    Changed Scanner popup

--- src/ScannerPopup.jsx
+++ src/ScannerPopup.jsx
@@ -1,30 +1,29 @@
-import React from "react";
+import React, {useEffect} from 'react';
 
 export default function QRScannerPopup() {
     const handleScan = () => {
         const tg = window.Telegram.WebApp;
 
         tg.showScanQrPopup(
-            { text: "Please scan your QR code" },
+            {text: 'Please scan your QR code'},
             (result) => {
                 if (result) {
                     alert(`Scanned QR: ${result}`);
                     tg.sendData(result);
                 } else {
-                    alert("QR scanning canceled");
+                    alert('QR scanning canceled');
                 }
-            }
+            },
         );
     };
 
+    useEffect(() => {
+        handleScan();
+    }, []);
+
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <button
-                onClick={handleScan}
-                className="px-4 py-2 text-white bg-blue-500 rounded"
-            >
-                Open Telegram QR Scanner
-            </button>
+        <div>
+
         </div>
     );
 }
